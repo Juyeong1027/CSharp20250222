708c3cc58f31f7862a7726e04bf07dbd6c778027
Juyeong1027 <ricky3045@naver.com>
2025-02-22 18:34:05 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices.Marshalling;
+using System.Xml.Serialization;
+
+namespace CSharpStudy20250222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
