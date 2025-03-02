e682175a95f0c393e483643b5511cf7324e85f21
Juyeong <ricky3045@naver.com>
2025-03-02 18:49:35 +0900

    2025-03-02 수업 자료

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using System.Xml.Serialization;
 
@@ -8,29 +9,100 @@ namespace CSharpStudy20250222
     {
         static void Main(string[] args)
         {
-            // 변수의 선언과 데이터 타입
-            // 정수형 데이터
-            // (데이터 타입) (변수 이름) = (값)
+            // 놀이동산 입장권
+            // 사용자로부터 나이, 군인 할인, 연간 회원 여부를 입력 받아서 입장 요금을 계산합니다.
 
-            Console.WriteLine("Hello, World!");
+            // 무료입장 조건
+            // 1. 나이가 3세 이하
+            // 2. 나이가 65세 이상
+            // 3. 연간 회원
 
-            string myName = "Hello";
-            Console.WriteLine(myName);
+            // 할인 적용
+            // 1. 군인 -> 30% 할인
 
-            int age = 25;
-            Console.WriteLine(age);
+            // 일반 요금
+            // 성인(19~64) : 10,000원
+            // 청소년(13~18) : 8,000원
+            // 어린이(4~12) : 5,000원
 
 
-            // 정수형 데이터
-            int myint = 25;
-            short myshort = 25;
-            long mylong = 25;
-            byte mybyte = 25;
+            //성인 할인 : 7,000원
 
-            Console.WriteLine(myint);
-            Console.WriteLine(myshort);
-            Console.WriteLine(mylong);
-            Console.WriteLine(mybyte);
+
+            // 출력 예시 
+            // 나이를 입력 하세요 : 20
+            // 군인인가요? (네/아니요) : 네
+            // 연간 회원인가요? (네/아니요) : 아니요
+            // 입장료 : '7,000'원 입니다
+
+            // 출력 예시 
+            // 나이를 입력 하세요 : 20
+            // 군인인가요? (네/아니요) : 네
+            // 연간 회원인가요? (네/아니요) : 네
+            // 입장료 : 무료 입장입니다.
+
+            string  agg,agg1,agg2;
+            int sh = 7000;
+
+
+
+            Console.Write("나이를 입력하세요 : ");
+            string ageInput = Console.ReadLine();
+
+            int age = int.Parse(ageInput);
+
+
+
+
+
+  
+
+            if (age >= 4 && age <= 12)
+            {
+                agg = "kids";
+            }
+            else if ( age >= 13 && age <= 18)
+            {
+                agg = "Student";
+            }
+            else if ( age >= 19 &&  age <= 64)
+            {
+                agg = "F";
+            }
+            else
+            {
+                Console.WriteLine("입장료 : 무료 입장입니다.");
+                return;
+            }
+
+            Console.Write("군인이신가요? (네/아니요) : ");
+            string army = Console.ReadLine();
+
+            Console.Write("연간 회원이신가요? (네/아니요) : ");
+            string yuser = Console.ReadLine();
+
+
+            if (yuser == "네")
+            {
+                agg2 = "yes";
+            }
+            else if(yuser == "아니요")
+            {
+                agg2 = "No";
+            }
+
+            if (yuser == "네")
+            {
+                Console.WriteLine("입장료 : 무료 입장입니다.");
+            }
+            else if (agg == "F" && army == "네")
+            {
+                Console.WriteLine($"입장료 :  ");
+            }
+
+
+
+
+        }
         }
     }
-}
